3cf77f303c9af35c705fcf201fe3204bf6021090
DouShaoxun <dousx_cn@163.com>
2026-08-18 10:49:58 +0800

    lib name

--- python/common.py
+++ python/common.py
@@ -1,6 +1,19 @@
 import os
-
+import sys
+import platform
 SOCKET_NAME = b"rust-ipc-lib-test"
 
+
+system = platform.system()
+if system == "Linux":
+    lib_name = "libipc_bridge"
+elif system == "Darwin":
+    lib_name = "ipc_bridge.dylib"
+elif system == "Windows":
+    lib_name = "ipc_bridge.dll"
+else:
+    print(f"不支持的平台: {system}")
+    sys.exit(1)
+
 dll_path = os.path.join(os.path.dirname(__file__), "..",
-                        "target", "release", "ipc_bridge.dll")
+                        "target", "release", lib_name)
